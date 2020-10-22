ed7f71fcf44d546d7a579502dd41917e36553da3
Jana Stastna <j.machkova@gmail.com)>
2020-10-22 13:10:52 +0200

    kliknuti kolecko, krizek plus nastylovani tridy css

--- script.js
+++ script.js
@@ -2,20 +2,30 @@
 
 console.log('funguje to!');
 
-let praveHraje = 'kolecko';
+let kdoHraje = 'kolecko';
 
-const ctvereckyElm = document.querySelector('.ctverecky');
-
-ctvereckyElm.addEventListener('click', () => {
-  if (praveHraje === 'kolecko') {
-    ctvereckyElm.classList.add('board__field--circle');
-    ctvereckyElm.style.backgroundColor = '';
-  } else if (praveHraje === 'krizek') {
-    ctvereckyElm.classList.add('board__field--cross');
+const zjistiTridu = () => {
+  if (kdoHraje === 'kolecko') {
+    return 'hrajeKolecko';
+  } else {
+    return 'hrajeKrizek';
   }
+};
+
+const ctverecekKlik = (e) => {
+  e.target.classList.add(zjistiTridu());
+};
+
+document.querySelectorAll('.ctverecky button').forEach((item) => {
+  item.addEventListener('click', ctverecekKlik);
 });
 
-/*
-obrázek kolečka a křížku - z předchozího úkolu
-css button nastylovat - aby měl background image kolečko / křížek
-*/
+const zmenHrace = () => {
+  if (kdoHraje === 'kolecko') {
+    kdoHraje = 'krizek';
+  } else {
+    kdoHraje = 'kolecko';
+  }
+};
+
+
